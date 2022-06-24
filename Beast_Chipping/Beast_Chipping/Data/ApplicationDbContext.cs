49fba1e9a3c38218fdc3e6395619d3c3f91f5bbe
Afonso Graca <aluno20944@ipt.pt>
2022-06-24 16:13:08 +0100

    teste1

---
+++ Beast_Chipping/Beast_Chipping/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Beast_Chipping.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Beast_Chipping/Beast_Chipping/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Beast_Chipping.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
